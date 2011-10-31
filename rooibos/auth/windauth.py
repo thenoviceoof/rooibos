2c5ee2579c3b2aed8802a3014013ba9372dd3445
Nathan Hwang <nyh2105@columbia.edu>
2011-10-31 14:32:00 -0400

    Starting to implement WIND

---
+++ rooibos/auth/windauth.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import User
+from django.conf import settings
+from baseauth import BaseAuthenticationBackend
+import logging
+
+class WindAuthenticationBackend(BaseAuthenticationBackend):
+    ### HAVE TO CHANGE THIS
+    ### CHECKOUT RemoteAuthBackend
+    def authenticate(self, username=None, password=None):
+        for ldap_auth in settings.LDAP_AUTH:
+            try:
+                username = username.strip()
+                l = ldap.initialize(ldap_auth['uri'])
+                l.protocol_version = ldap_auth['version']
+                for option, value in ldap_auth['options'].iteritems():
+                    l.set_option(getattr(ldap, option), value)
+                l.simple_bind_s('%s=%s,%s' % (ldap_auth['cn'], username, ldap_auth['base']), password)
+                result = l.search_s(ldap_auth['base'],
+                                    ldap_auth['scope'],
+                                    '%s=%s' % (ldap_auth['cn'], username),
+                                    attrlist=ldap_auth['attributes'])
+                if (len(result) != 1):
+                    continue
+                attributes = result[0][1]
+                for attr in ldap_auth['attributes']:
+                    if not type(attributes[attr]) in (tuple, list):
+                        attributes[attr] = (attributes[attr],)
+                try:
+                    user = User.objects.get(username=username)
+                except User.DoesNotExist:
+                    user = self._create_user(username,
+                                      None,
+                                      ' '.join(attributes[ldap_auth['firstname']]),
+                                      ' '.join(attributes[ldap_auth['lastname']]),
+                                      attributes[ldap_auth['email']][0])
+                if not self._post_login_check(user, attributes):
+                    continue
+                return user
+            except ldap.LDAPError, error_message:
+                logging.debug('LDAP error: %s' % error_message)
+            finally:
+                if l:
+                    l.unbind_s()
+        return None
