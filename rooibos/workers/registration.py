d3d46a7c5848e10c8bf938f46f7e9a3fc55d820e
Nathan Hwang <beta.entity.k@gmail.com>
2012-04-06 14:13:11 -0400

    Tried changing the rooibos code to use the updated gearman

--- rooibos/workers/registration.py
+++ rooibos/workers/registration.py
@@ -1,12 +1,14 @@
 import sys
 from django.conf import settings
-from gearman import Task, GearmanWorker, GearmanClient
-from gearman.task import Taskset
+from gearman import GearmanWorker, GearmanClient
 
+# misnomer, local dictionary keeping track of work tasks
 workers = dict()
 
-client = settings.GEARMAN_SERVERS and GearmanClient(settings.GEARMAN_SERVERS) or None
-
+if settings.GEARMAN_SERVERS:
+    client = GearmanClient(settings.GEARMAN_SERVERS)
+else:
+    client = None
 
 def register_worker(id):
     def register(worker):
@@ -14,8 +16,8 @@ def register_worker(id):
         return worker
     return register
 
-
 def discover_workers():
+    """Populates the workers dict"""
     if not workers:
         for app in settings.INSTALLED_APPS:
             try:
@@ -23,27 +25,26 @@ def discover_workers():
             except ImportError:
                 pass
 
-
 def create_worker():
+    """Create a gearman worker, register all possible functions with it"""
     discover_workers()
     worker = GearmanWorker(settings.GEARMAN_SERVERS)
     for id, func in workers.iteritems():
-        worker.register_function(id, func)
+        worker.register_task(id, func)
     return worker
-
 
 def run_worker(worker, arg, **kwargs):
     discover_workers()
-    task = Task(worker, arg, **kwargs)
     if client:
-        if task.background:
-            taskset = Taskset([task])
-            client.do_taskset(taskset)
-            return task.handle
+        request = client.submit_job(worker, arg)
+        # specific check if we want it asynchronously or not
+        if kwargs.get("background", None):
+            return request
         else:
-            return client.do_task(task)
+            return request.result
     else:
+        # presume this assumes workers is a set of functions
         if workers.has_key(worker):
-            return workers[worker](task)
+            return workers[worker](dict(task=worker, data=arg))
         else:
             raise NotImplementedError()
